2cbb8b5b5015c323b64c5ad1f45adcde2e13dab6
Andy <andreyzp87@gmail.com>
2024-08-31 23:35:59 +0300

    implement task

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
+
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+function App() {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    return savedContacts === null ? initialContacts : JSON.parse(savedContacts);
+  });
+
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = contact => {
+    setContacts([contact, ...contacts]);
+  };
+
+  const visibleContacts = contacts.filter(contact =>
+    filter ? contact.name.toLowerCase().includes(filter.toLowerCase()) : true
+  );
+
+  const deleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
+
+  return (
+    <div className="app">
+      <h1>Phonebook</h1>
+      <ContactForm onCreate={addContact} />
+      <SearchBox onSearch={setFilter} />
+      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
+    </div>
+  );
+}
+
+export default App;
